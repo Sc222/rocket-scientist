5bdddba9b4ac093e1fef0c64e86bb9c8a6f9d3ce
Sc222 <v.a.shaternikov@gmail.com>
2020-07-08 13:16:56 +0500

    stage 2 bug fixes and add hp bar

--- SecondStage.gd
+++ SecondStage.gd
@@ -1,17 +1,16 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# todo число попыток + интерфейс с попытками
 var LAST_TASK=5
+var TASK_HP_BONUS=2
 var TASKS_SOLVED="Поздравляем, ракета спроектирована!"
+
+var hp = 2
 var currentTask = 1
 var currentTaskText = ""
 var userAnswer = ""
 var isTaskGenerated = true
 onready var taskLabel = get_node("TaskContainer/Task")
+onready var hpLabel = get_node("HpText")
 onready var rocket = get_node("rocket")
 
 
@@ -26,6 +25,7 @@ var tasks_dict = {
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	UpdateHp(0)
 	GenerateNewTask()
 
 func _on_Answer_text_changed(new_text):
@@ -38,8 +38,12 @@ func _on_rocket_animation_finished():
 		FinishStage()
 		
 
+func UpdateHp(changeValue):
+	hp=hp+changeValue
+	hpLabel.set_text(str(hp))
+
 func FinishStage():
-	taskLabel.set_text("Поздравляем, задачи решены.")
+	taskLabel.set_text("Поздравляем, ракета спроектирована.")
 	#todo сделать кнопку "следующий этап" и сделать ее видимой
 
 func GenerateNewTask():
@@ -51,25 +55,36 @@ func GenerateNewTask():
 	taskLabel.set_text(currentTaskText)
 
 func NextTask():
+		get_node("task_"+str(currentTask)).play("complete")
+		get_node("task_"+str(currentTask)+"_line").play("complete")
+		if currentTask!=LAST_TASK:
+			isTaskGenerated=false
+			get_node("task_"+str(currentTask+1)).play("unlock")
+			get_node("task_"+str(currentTask+1)+"_line").play("unlock")
+		rocket.play("task_"+str(currentTask))
+		currentTask=currentTask+1
+
+func SetError():
+	# todo вычитать попытки
+	var taskButton = get_node("task_"+str(currentTask))
+	var taskLine = get_node("task_"+str(currentTask)+"_line")
+	taskButton.frame = 0
+	taskLine.frame = 0
+	taskButton.play("wrong")
+	taskLine.play("wrong")
 	
-	get_node("task_"+str(currentTask)).play("complete")
-	get_node("task_"+str(currentTask)+"_line").play("complete")
-	if currentTask!=LAST_TASK:
-		isTaskGenerated=false
-		get_node("task_"+str(currentTask+1)).play("unlock")
-		get_node("task_"+str(currentTask+1)+"_line").play("unlock")
-	rocket.play("task_"+str(currentTask))
-	currentTask=currentTask+1
 
 func CheckAnswer():
-	if tasks_dict[currentTaskText] == userAnswer:
-		print("correct")
-		get_node("Answer").set_text("")
-		NextTask()
-	else:
-		# todo вычитать попытки
-		print("вычитать попытки")
-		get_node("task_"+str(currentTask)).play("wrong")
-		get_node("task_"+str(currentTask)+"_line").play("wrong")
+	if currentTask!=LAST_TASK+1:
+		if tasks_dict[currentTaskText] == userAnswer:
+			UpdateHp(TASK_HP_BONUS)
+			print("correct")
+			get_node("Answer").set_text("")
+			NextTask()
+		else:
+			UpdateHp(-1)
+			# todo вычитать попытки
+			print("вычитать попытки")
+			SetError()
 
 	
