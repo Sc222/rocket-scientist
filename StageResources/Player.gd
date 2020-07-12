d3894da3feb7845acc98dede53b7c0a6c03aec4f
Sc222 <v.a.shaternikov@gmail.com>
2020-07-12 15:55:34 +0500

    player animation, gun assets and invisible walls around the map

--- StageResources/Player.gd
+++ StageResources/Player.gd
@@ -1,22 +1,36 @@
 extends KinematicBody2D
 
 export var speed = 16*20
+#const DIR_LEFT = "l"
+#const DIR_RIGHT="r"
+#var direction=DIR_RIGHT
+onready var sprite = get_node("Sprite")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 func _physics_process(delta):
-	# Get player input
-	var direction: Vector2
-	direction.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
-	direction.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
+	var dir_vector: Vector2
+	dir_vector.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	dir_vector.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	
-	# If input is digital, normalize it for diagonal movement
-	if abs(direction.x) == 1 and abs(direction.y) == 1:
-		direction = direction.normalized()
+	if abs(dir_vector.x) == 1 and abs(dir_vector.y) == 1:
+		dir_vector = dir_vector.normalized()
+		
+	var movement = speed * dir_vector * delta
 	
-	# Apply movement
-	var movement = speed * direction * delta
+	update_animation(dir_vector.x, movement.length()!=0)	
+	
 	move_and_collide(movement)
+
+func update_animation(x_direction, is_moving):
+	print(x_direction)
+	print(x_direction<0)
+	if x_direction>0:
+		sprite.set_flip_h(false)
+	elif x_direction<0:
+		sprite.set_flip_h(true)
+	if is_moving:
+		sprite.play("run")
+	else:
+		sprite.play("idle")
