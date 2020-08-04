2109ea77ece9ced98711fb693c4436e21d393ced
Sc222 <v.a.shaternikov@gmail.com>
2020-08-04 20:38:29 +0500

    remove example project

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -4,13 +4,23 @@ const BG_SPEED = -50
 onready var ParallaxBg = get_node("ParallaxBackground")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(delta)
 	ParallaxBg.scroll_offset.x+=BG_SPEED*delta
 	pass
+
+func launch_game():
+	pass
+
+func launch_leaderboards():
+	# todo launch leaderboards
+	pass
+
+func launch_credits():
+	# todo launch credits
+	pass
+
+func exit():
+	get_tree().quit()
